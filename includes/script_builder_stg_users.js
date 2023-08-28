09acd5f5b14c851a3ba56906759510787d2040fb
cissy.lu@artefact.com <auth0|64eb06bc3632e84f284d8a8a>
2023-08-28 10:43:58 +0000

    add reusable constants and sql templates

---
+++ includes/script_builder_stg_users.js
@@ -0,0 +1,22 @@
+// Read more in the JS API documentation here: https://docs.dataform.co/guides/javascript/js-api
+
+function render_sql(min_dt) {
+    let sql_template = `
+        SELECT id
+               , first_name
+               , last_name
+               , email
+               , age
+               , gender
+               , city
+               , country
+               , traffic_source
+               , create_date
+        FROM ${ref({name: "users", schema: "raw_ecom"})}
+        WHERE create_date > "${min_dt}";
+    `;
+
+    return sql_template
+}
+
+module.exports = { render_sql }	
